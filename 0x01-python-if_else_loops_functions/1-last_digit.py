0650b2d01e5de5cd799b9941c5d2a7250fbdfa4f
saif-sabit <saifsabit@gmail.com>
2023-06-06 07:21:23 -0700

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,4 @@ elif int(n) == 0:
     cond = "and is 0"
 else:
     cond = "and is less than 6 and not 0"
-print(f"Last digit of {number:d} is {str(number)[-1]} {cond}")
+print(f"Last digit of {number:d} is {n:d} {cond}")
