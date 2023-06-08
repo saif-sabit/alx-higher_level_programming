28cbb11a18eb9b80b61ab1245c648d56c92c6ae5
saif-sabit <saifsabit@gmail.com>
2023-06-08 09:36:25 -0700

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-add_0.add(a, b)
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
 
