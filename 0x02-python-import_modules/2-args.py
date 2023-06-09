de53246f59c6b1f6edf3e7b0944b8626b173aaa9
saif-sabit <saifsabit@gmail.com>
2023-06-08 18:41:41 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    ''' print args '''
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} arguments:".format(args))
+    else:
+        print("{} arguments:".format(args))
+    for i in range(args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
