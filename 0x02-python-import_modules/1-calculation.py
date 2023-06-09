77bb5ea4b0224e3639d7d6589284215ae6cbb941
saif-sabit <saifsabit@gmail.com>
2023-06-08 19:49:52 -0700

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     ''' print sum and sub and mul and div of a and b '''
-    from calculator_1  import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
