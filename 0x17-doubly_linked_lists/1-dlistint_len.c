bd6813ff76f2f9f8717c9103ab1afcead3116b44
saif-sabit <saifsabit@gmail.com>
2023-06-15 00:52:54 -0700

    Task 0 & 1 Betty checked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,17 @@
 #include "lists.h"
 /**
- * print_dlistint - prints list contents
+ * dlistint_len - prints list size
  * @h: pointer to a doubbly list
  * Return: size of printed list
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
 	return (i);
 }
-
