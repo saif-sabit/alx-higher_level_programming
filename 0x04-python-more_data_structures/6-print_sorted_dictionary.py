fd595baab3fa0b089a211e55a965b2219f6df356
saif-sabit <saifsabit@gmail.com>
2023-06-16 02:12:29 -0700

    Fault may be in other files **print sorted file**

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    k_list = list(a_dictionary.keys())
-    k_list.sort()
-    for i in k_list:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+    if (a_dictionary):
+        k_list = list(a_dictionary.keys())
+        k_list.sort()
+        for i in k_list:
+            print("{}: {}".format(i, a_dictionary.get(i)))
