78a431680fe7ad835a3a9d8e7db3549e8eb238e3
saif-sabit <saifsabit@gmail.com>
2023-06-16 06:03:25 -0700

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        x = list(a_dictionary.values())[0]
+        for key, value in a_dictionary.items():
+            if value > x:
+                ret = key
+        return ret
+    else:
+        return None
