e32e1668ab92d20dc3e8179831f81a1835cca24e
saif-sabit <saifsabit@gmail.com>
2023-07-06 01:18:29 +0300

    3 again

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -61,5 +61,5 @@ class Rectangle:
             for i in range(self.__height):
                 for j in range(self.__width):
                     shape.append('#')
-            shape.append("\n")
+                shape.append("\n")
         return ("".join(shape))
