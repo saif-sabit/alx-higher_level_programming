efe03da2a3c93db6fd9f338e903cf41487fff7ba
saif-sabit <saifsabit@gmail.com>
2023-07-06 02:13:11 +0300

    Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -80,6 +80,7 @@ class Rectangle:
         """delection message"""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return greater area retangle.
@@ -97,3 +98,4 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
+    
