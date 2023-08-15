55059a05357f74454197a2a8d31e42dde63df654
saif-sabit <saifsabit@gmail.com>
2023-08-15 13:01:31 +0300

    SQL intro

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+insert into first_table values(id,name) values(89,'Best School');
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,1 @@
+-- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
