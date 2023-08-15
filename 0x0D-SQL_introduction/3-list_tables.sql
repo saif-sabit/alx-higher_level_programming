e3d91c7bcead2e44344c1769170f8adc1f54c105
saif-sabit <saifsabit@gmail.com>
2023-08-15 17:34:09 +0300

    3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
 -- lists all the tables of a database in your MySQL server.
 SELECT table_name
-FROM INFORMATION_SCHEMA.TABLES
-WHERE table_type = 'BASE TABLE'
+FROM information_schema.tables
+WHERE table_type='BASE TABLE'
