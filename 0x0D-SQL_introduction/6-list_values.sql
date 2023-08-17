68b63063bb3525b11f6f7aaa731c9b4442d1c551
saif-sabit <saifsabit@gmail.com>
2023-08-17 10:20:31 +0300

    update

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
---a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL serve
-SELECT * FROM 'first_table';
+--a script that lists all rows
+SELECT * FROM first_table;
