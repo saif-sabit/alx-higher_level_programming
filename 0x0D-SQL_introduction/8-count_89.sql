d148a1e7c8fc4fc6fd26fdea283c3513c6f7d927
saif-sabit <saifsabit@gmail.com>
2023-08-17 12:29:17 +0300

    08

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- a script that SELECT the number of records with id = 89 in the table first_table
-SELECT  count(*) from first_table WHERE id = 89;
+SELECT COUNT(id) from first_table WHERE id = 89;
