461229627f2544d1cea3f5f5c38726eb00be95d4
saif-sabit <saifsabit@gmail.com>
2023-09-06 12:39:09 +0300

    Task 3 & 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- Create table id_not_null with not null and default properites 
-CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name (VARCHAR(256)));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
