cefe304290a19c9e7ce9325c8ffb676691be7af5
saif-sabit <saifsabit@gmail.com>
2023-09-06 13:01:02 +0300

    Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- Create database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+-- Use our database
+use hbtn_0d_usa;
+-- Create table states 
+CREATE TABLE IF NOT EXISTS states(id INT NOT NULL PRIMARY KEY, name NOT NULL VARCHAR(256));
