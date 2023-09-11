9a35a660a78b6fde631510821c0bbbbf9f933ae7
saif-sabit <saifsabit@gmail.com>
2023-09-11 07:32:16 -0700

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-
-// print process.argv
-if (argv.length > 1) {
-        console.log("No argument");
+if (process.argv.length === 2) {
+        console.log('No argument');
 } else {
-        console.log("Argument found");
+        console.log('Argument found');
 }
