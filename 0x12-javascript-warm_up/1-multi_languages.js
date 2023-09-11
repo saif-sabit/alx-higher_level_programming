711b2876f629e7409e5488df5f356f110298d1b5
saif-sabit <saifsabit@gmail.com>
2023-09-11 07:21:59 -0700

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log("C is fun" + "\n" + "Python is cool" + "\n" + "JavaScript is amazing");
+console.log('C is fun' + '\n' + 'Python is cool' + '\n' + 'JavaScript is amazing');
