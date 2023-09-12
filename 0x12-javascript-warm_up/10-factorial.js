543ce8373eef01877d691eeac1b5a6962e12e847
saif-sabit <saifsabit@gmail.com>
2023-09-12 17:05:31 +0300

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 function facrtorial(a){
+        if (a ===0){
+                return (1);
+        }
+        return a * facrtorial(a - 1);
 }
+facrtorial(parseInt(process.argv[2]));
