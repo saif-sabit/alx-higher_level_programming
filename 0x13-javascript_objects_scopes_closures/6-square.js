befc63b631497ebeda25132f81d1b7f6704cbf70
saif-sabit <saifsabit@gmail.com>
2023-09-13 01:56:32 +0300

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+class Square extends Square1 {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        if (c) {
+          s += 'c';
+        } else {
+          s += 'X';
+        }
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Square;
