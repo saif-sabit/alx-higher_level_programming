b18619e6b31f034cc21addbf543662c65f0ad57f
saif-sabit <saifsabit@gmail.com>
2023-09-14 07:36:46 -0700

    Code style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,12 +2,13 @@
 """ script that lists all states starts with N """
 import MySQLdb
 import sys
-if __name__ =="__main__":
-  db =  MySQLdb.connect("localhost",user = sys.argv[1],password = sys.argv[2], db = sys.argv[3], port=3306)
-  c = db.cursor()
-  c.execute("SELECT * FROM states where name LIKE'N%' ORDER BY id")
-  rows = c.fetchall()
-  for row in rows:
-    print(row)
-  c.close()
-  db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states where name LIKE'N%' ORDER BY id")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,13 +2,14 @@
 """ script that lists all states filtered by user input """
 import MySQLdb
 import sys
-if __name__ =="__main__":
-  db =  MySQLdb.connect("localhost",user = sys.argv[1],password = sys.argv[2], db = sys.argv[3], port=3306)
-  c = db.cursor()
-  sql = "SELECT * FROM states WHERE name = '{name}' ORDER BY id"
-  c.execute(sql.format(name = sys.argv[4]))
-  rows = c.fetchall()
-  for row in rows:
-    print(row)
-  c.close()
-  db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    sql = "SELECT * FROM states WHERE name = '{name}' ORDER BY id"
+    c.execute(sql.format(name=sys.argv[4]))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
