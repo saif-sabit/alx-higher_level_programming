e78c637fb41967c8ea3262c7438a4760c2459efc
saif-sabit <saifsabit@gmail.com>
2023-09-16 01:04:17 -0700

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,12 +2,14 @@
 """ script that lists all states filtered by user input """
 import MySQLdb
 import sys
-if __name__ =="__main__":
-  db =  MySQLdb.connect("localhost",user = sys.argv[1],password = sys.argv[2], db = sys.argv[3], port=3306)
-  c = db.cursor()
-  c.execute(" SELECT * FROM states  WHERE name LIKE %s ORDER BY id",(sys.argv[4],))
-  rows = c.fetchall()
-  for row in rows:
-    print(row)
-  c.close()
-  db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute(" SELECT * FROM states  WHERE name LIKE %s ORDER BY id",
+              (sys.argv[4],))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
