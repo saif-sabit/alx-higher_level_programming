0bb059121f1f7d52dfd5b646a56e80d79fbf0e91
saif-sabit <saifsabit@gmail.com>
2023-09-16 01:46:17 -0700

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect("localhost", user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON\
-             cities.state_id = states.id ORDER BY cities.id")
+    c.execute("SELECT cities.id, cities.name, states.name FROM\
+            cities LEFT JOIN states ON cities.state_id = states.id\
+            ORDER BY cities.id")
     rows = c.fetchall()
     for row in rows:
         print(row)
