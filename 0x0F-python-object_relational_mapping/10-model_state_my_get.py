914df0a8b2cea9da1aea5067af1610a8c019c61c
saif-sabit <saifsabit@gmail.com>
2023-09-17 07:21:53 -0700

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Fetch ALl states"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    session = Session()
+    state = session.query(State).filter(
+            State.name.like('{}'.format(sys.argv[4]))).first()
+    if state is None:
+        print("Not found")
+    else:
+        print("{}".format(state.id))
+    session.close()
